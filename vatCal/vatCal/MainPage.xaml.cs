6f88c39129dc2d375a199afb98787c6047793936
train18 <train18>
2016-08-24 12:12:55 -0700

    first commit

---
+++ vatCal/vatCal/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace vatCal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+           
+        } 
+
+        void Calculate(object sender, EventArgs e)
+        {
+            string money = entryMoney.Text;
+            double result = Double.Parse(money)*107 / 100;
+            labelResult.Text = result.ToString();
+        }
+
+    }
+}
